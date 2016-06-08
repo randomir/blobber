772bb91862fca1941a17df59eee7c199d91330c9
Radomir Stevanovic <radomir.stevanovic@gmail.com>
2016-06-08 04:15:18 +0200

    knot insert and remove (with double click)

--- www/js/main.js
+++ www/js/main.js
@@ -24,10 +24,21 @@ function getControlPoints(x0,y0,x1,y1,x2,y2,t){
     return [p1x,p1y,p2x,p2y];
 }
 
+function dist2(x1, y1, x2, y2) {
+    return Math.pow(x2-x1, 2) + Math.pow(y2-y1, 2);
+}
+
+function min(a, b) {
+    return a < b ? a : b;
+}
+function max(a, b) {
+    return a > b ? a : b;
+}
+
 
 $(function() {
     var canvas = Raphael("blob", 800, 800);
-    var points = [[400, 200], [600, 400], [400, 600], [200, 400]], pointRadius = 5,
+    var initialPoints = [[400, 200], [600, 400], [400, 600], [200, 400]], pointRadius = 5,
         pointAttr = {fill: "#f00", stroke: "#000", "stroke-width": 3},
         curveAttr = {fill: "#f00", stroke: "#000", "stroke-width": 3},
         knots = [];
@@ -39,14 +50,31 @@ $(function() {
         this.attr({cx: evt.offsetX, cy: evt.offsetY});
         redrawPath();
     }
+    function knotRemove(dx, dy, x, y, evt) {
+        for (var i = 0; i < knots.length; i++) {
+            if (knots[i] == this) {
+                this.remove();
+                knots.splice(i, 1);
+                break;
+            }
+        }
+        redrawPath();
+    }
 
     // render curve between knots
+    var pts = [];
     function redrawPath() {
-        if (knots.length < 5) return;
-        var pts = [];
-        for (var i = 0; i < knots.length; i++) {
-            pts.push(knots[i].attr("cx"), knots[i].attr("cy"));
+        if (knots.length < 3) return;
+        // close the path by adding first/last knot to end/beginning
+        var kts = knots.slice();
+        kts.push(kts[0]);
+        kts.unshift(kts[kts.length-2]);
+
+        pts = [];
+        for (var i = 0; i < kts.length; i++) {
+            pts.push(kts[i].attr("cx"), kts[i].attr("cy"));
         }
+
         var cps = [];
         for (var i = 0; i <= pts.length-6; i += 2) {
             cps = cps.concat(getControlPoints(pts[i], pts[i+1], pts[i+2], pts[i+3], pts[i+4], pts[i+5], tension));
@@ -61,16 +89,49 @@ $(function() {
         curve.attr({path: path.join(" ")}).toBack();
     }
 
-    // create knots, draw them on canvas
-    for (var i = 0; i < points.length; i++) {
-        var point = points[i];
-        var knot = canvas.circle(point[0], point[1], pointRadius).attr(pointAttr);
-        knot.drag(knotMove);
-        knots.push(knot);
+    function closestKnot(x, y) {
+        var minDist = 1e10, minIdx = -1;
+        for (var i = 0; i < knots.length; i++) {
+            var xi = knots[i].attr("cx"), yi = knots[i].attr("cy"), d = dist2(x, y, xi, yi);
+            if (d < minDist) {
+                minDist = d;
+                minIdx = i;
+            }
+        }
+        if (minIdx < 0) return;
+        return minIdx;
     }
-    // close the path by adding first/last knot to end/beginning
-    knots.push(knots[0]);
-    knots.unshift(knots[knots.length-2]);
+
+    function nextClosestKnot(idx, x, y) {
+        var n = knots.length;
+        var next = (idx + 1) % n;
+        var prev = (idx - 1 + n) % n;
+        if (dist2(x, y, knots[prev].attr("cx"), knots[prev].attr("cy")) < 
+            dist2(x, y, knots[next].attr("cx"), knots[next].attr("cy"))) {
+            return prev;
+        } else {
+            return next;
+        }
+    }
+
+    // create and insert into knots at pos idx
+    function createKnot(x, y, idx) {
+        var knot = canvas.circle(x, y, pointRadius).attr(pointAttr);
+        knot.drag(knotMove);
+        knot.dblclick(knotRemove);
+        if (idx) {
+            knots.splice(idx, 0, knot);
+        } else {
+            knots.push(knot);
+        }
+        return knot;
+    }
+
+    // create knots, draw them on canvas
+    for (var i = 0; i < initialPoints.length; i++) {
+        var point = initialPoints[i];
+        createKnot(point[0], point[1]);
+    }
 
     curve = canvas.path("M 0 0").attr(curveAttr);
 
@@ -82,6 +143,15 @@ $(function() {
         } else {
             tension += 0.1;
         }
+        redrawPath();
+    });
+
+    $("#blob path").on("dblclick", function(e) {
+        var x = e.offsetX, y = e.offsetY;
+        var i = closestKnot(x, y);
+        var j = nextClosestKnot(i, x, y);
+        var wraps = (max(i,j) + 1) % knots.length == min(i,j);
+        createKnot(x, y, !wraps && max(i, j));
         redrawPath();
     });
 });
